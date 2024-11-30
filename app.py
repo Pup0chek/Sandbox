8e9efd89b3c8df7d6f7f8f245e841f67c5b605b2
lia <martynovaliya@gmail.com>
2024-11-30 17:17:38 +0300

    test

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import base64
+
 from flask import Flask, render_template
+from core.VirusTotalAPI import Upload_file, Get_File_Info
 
 app = Flask(__name__)
 
@@ -6,7 +9,12 @@ app = Flask(__name__)
 #функция представления
 @app.route('/index/')
 def index():
-    return render_template('..\\index')
+    id = Upload_file("C:\\Sandbox\\test.txt").get("id")
+    decoded = base64.b64decode(id).decode('utf-8')
+    print(decoded)
+    splited = decoded.split(":")
+    message = Get_File_Info(splited[0])
+    return render_template('index.html', message=message)
 
 @app.route('/<int:num>/')
 def lol(num: int):
--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import base64
 from core.VirusTotalAPI import Upload_file, Get_File_Info
 
 
-
-
-
 def main():
         id = Upload_file("test.txt").get("id")
         decoded = base64.b64decode(id).decode('utf-8')
