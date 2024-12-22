bf730fbf067b271fe0a4c5df2945ef3424a12b68
lia <martynovaliya@gmail.com>
2024-12-22 19:39:59 +0300

    blueprints

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 
-# Создаем Blueprint
+
 auth = Blueprint('auth', __name__, template_folder='templates')
 
 users = {"admin": "password123"}
