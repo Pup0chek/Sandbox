baffdf186bde75a2e90646bf1473e78132d8c3bb
Lia <martynovaliya@gmail.com>
2024-12-24 12:36:53 +0300

    simple registration

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -8,6 +8,7 @@ users = {"admin": "password123"}
 @auth.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
+        print(users)
         username = request.form.get("username")
         password = request.form.get("password")
         if username in users and users[username] == password:
@@ -23,3 +24,22 @@ def logout():
     session.pop("username", None)
     flash("Вы вышли из системы.", "info")
     return redirect(url_for("index"))
+
+@auth.route("/register/", methods = ['post', 'get'])
+def register():
+    if request.method == "POST":
+        print(users)
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+        if username not in users:
+            users[username] = password
+            session["username"] = username
+            if confirm_password == password:
+                    flash("Вы успешно зарегистрировались!", "success")
+                    return redirect("/")
+            flash("Пароли не сходятся!", "danger")
+        flash("Пользователь с таким именем уже есть!", "danger")
+
+
+    return render_template("register.html")
